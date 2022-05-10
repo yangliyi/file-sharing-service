fea51e9d6fe02ac027ae4150442db13b235f3817
yangliyi <yangliyi1228@gmail.com>
2022-05-10 09:08:01 +0800

    STDOUT for log and move token to header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::API
 
   def authenticate_user_token
-    user = User.find_by(auth_token: params[:auth_token])
+    puts "Authorization#{request.headers['Authorization']}"
+
+    user = User.find_by(auth_token: request.headers['Authorization'])
     return render(json: { message:'invalid token' }, status: 401) if user.nil?
 
     sign_in(user, store: false)
